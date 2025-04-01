3fa6111d12b557f7fd74d9ac1aa52864c3ca4422
Gagan <135157712+gagan-1211@users.noreply.github.com>
2025-04-02 00:29:22 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,14 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/9.x.x/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.x.x/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCqg-qBk_iv0sE2l5Pv_ciSPotFG0eZNRQ",
-    authDomain: "alert-63f70.firebaseapp.com",
-    databaseURL: "https://alert-63f70-default-rtdb.firebaseio.com",
-    projectId: "alert-63f70",
-    storageBucket: "alert-63f70.appspot.com",
-    messagingSenderId: "328076619196",
-    appId: "1:328076619196:web:bbc00ccee8121c9bd30fb9",
-    measurementId: "G-W2FBCZB43S"
+    
 };
 
 firebase.initializeApp(firebaseConfig);
